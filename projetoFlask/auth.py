f244cf63ea9a7e4fea8302eeb7452f880f61077b
Flávio Júnior <fajuol2@gmail.com>
2024-03-11 17:29:42 -0300

    feat: arquivo de configuração de autenticação

---
+++ projetoFlask/auth.py
@@ -0,0 +1,66 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db  
+from flask_login import login_user, login_required, logout_user, current_user
+
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Autenticação feita com sucesso!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Senha incorreta, tente novamente.', category='error')
+        else:
+            flash('Email não cadastrado', category='error')
+
+    return render_template("login.html", user=current_user)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email já cadastrado.', category='error')
+        elif len(email) < 4:
+            flash('O email precisa ter mais de 3 caracteres', category='error')
+        elif len(name) < 2:
+            flash('O nome precisa ter mais de 1 caracteres.', category='error')
+        elif password1 != password2:
+            flash('As senhas não coincidem', category='error')
+        elif len(password1) < 7:
+            flash('A senha precisa ter mais de 7 caracteres.', category='error')
+        else:
+            new_user = User(email=email, name=name, password=generate_password_hash(
+                password1, method='scrypt'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('Cadastro concluido!', category='success')
+            return redirect(url_for('views.home'))
+
+    return render_template("sign_up.html", user=current_user)
